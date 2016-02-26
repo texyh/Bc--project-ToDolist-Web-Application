b085d438a3751680cb47719a2ea17bcb477b0f89
onwuzulike emeka <onwuzulike emeka>
2016-02-26 14:17:37 +0100

    my third commit

--- app.py
+++ app.py
@@ -1,60 +1,76 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash 
-from todolist import ToDoList
+from flask import Flask, render_template, redirect, \
+url_for, request, session, flash, g 
 from functools import wraps
+import sqlite3
+ ''' first i inititalized the flask object
+     then i set the secret key for managing session
+     the i created and instance of the database module
+ '''
+
 
 app = Flask(__name__)
 app.secret_key = "emeka"
+app.database = "todo.db"
 
+#the loginrequired decorator
 def login_required(f):
 	@wraps(f)
 	def wrap(*args, **kwargs):
 		if "logged_in" in session:
 			return f(*args, **kwargs)
 		else:
-			flash('You need to login first')
 			return redirect(url_for('signin'))
 	return wrap
 
-
+#the signup decorator
 @app.route('/', methods = ['GET','POST'])
 def signup():
-	error = None
-	if request.method  == 'POST':
-		if request.form['email']!= 'admin':#or request.form['password']!= 'admin' :
-			 error = 'Email is already found in our database'
-		else:
-			session["logged_in"] = True
-			return redirect(url_for('inbox'))	
-	return render_template('index.html', error=error )
+    error = None
+    if request.method  == 'POST':
+        g.db = connect_db()
+        cur = g.db.execute('select * from user')
+        for i in cur:
+            if i[2]==request.form['email']:
+                error ="Email exist in our datebase"    
+            else:
+                g.db.execute('INSERT INTO user VALUES("request.form[firstname]",\
+                    "request.form[lastname]","request.form[email]","request.form[password]")')
+                session["logged_in"] = True
+                return redirect(url_for('inbox'))
+    return render_template('index.html', error=error )
 
-
+#the signin decorator
 @app.route('/welcome',methods = ['GET','POST'])
 def signin():
-	error = None
-	#import pdb;pdb.set_trace()
-	if request.method  == 'POST':
+    error = None
+    if request.method  == 'POST':
+        g.db = connect_db()
+        cur = g.db.execute('select * from user')
+        for i in cur:
+            if i[2]==request.form['email'] and i[3] == request.form['password']:
+                session["logged_in"] = True
+                return redirect(url_for('inbox'))
+            else:
+                error = 'Invalid credentials. Please try again.'
+    return render_template('welcome.html')
 
-		if request.form['username']!= 'admin' or \
-		request.form['password']!= 'admin' :
-			 error = 'Invalid credentials. Please try again.'
-		else:
-			session["logged_in"] = True
-			flash('you were just logged in!')
-			return redirect(url_for('inbox'))
-	return render_template('welcome.html')
-
-
+#the loginrequired decorator
 @app.route('/inbox',methods = ['GET','POST'])
 @login_required
 def inbox():
 	return render_template('inbox.html')
 
-
+#the logout decorator
 @app.route('/logout')
 def logout():
 	session.pop('logged_in',None)
-	flash('you were just logged out!')
 	return redirect(url_for('signup'))
+
+
+def connect_db():
+    return sqlite3.connect(app.database)
+
+
 
 
 if __name__ == "__main__":
